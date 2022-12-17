3b4bd81c32f00ef6f358fafb821bc4ee0ba76ee4
Brendan Perry <brendan@brendanperry.me>
2022-12-17 13:56:21 -0500

    backing up

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+git add .
+git commit -m "backing up"
+git push
 java -Xms512M -Xmx512M -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -jar velocity.jar
