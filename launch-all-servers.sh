d51415b48571642652eaaf6f40679c2a93e439c9
Brendan Perry <brendan@brendanperry.me>
2023-09-14 01:07:05 -0400

    backing up

--- launch-all-servers.sh
+++ launch-all-servers.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-cd ~/Minecraft/mc-frens/
-./start.sh > output.log &
+tmux new-window -n:mcfrens 'cd ~/Minecraft/mc-frens/; ./start.sh'
 
-cd ../minecraft-frens-1.18/
-./start.sh > output.log &
+tmux new-window -n:survival 'cd ~/Minecraft/minecraft-frens-1.18/; ./start.sh'
 
-#cd ../minecraft-frens-1.19/
-#./start.sh > output.log &
+tmux new-window -n:moddedsurvival 'cd ~/Minecraft/better-mc/; ./start.sh'
 
-cd ../better-mc/
-./start.sh > output.log &
+tmux new-window -n:lobby 'cd ~/Minecraft/mc-frens-creative/; ./start.sh'
 
-cd ../mc-frens-creative/
-./start.sh > output.log &
